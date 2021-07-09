7e85f8a49b9dd37043d51fb1f8d39e0b249466ea
i37406 <waqas.ciitwah@gmail.com>
2021-07-10 01:27:11 +0500

    Set Views according to our project

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Commits Detail:
 1. Add Comment Section : A simple task to understand the working of Livewire. User can add comment & this comment is stored in an array and show on main page from this array by using livewire.
 2. Convert input to form : Change the properties of wire model and convert the input to submit as form.
 3. Intall Laravel Breeze : Install this package to make user and their relation b/w comments ie. A user can create multiple comments.
+4. Setup Views : Set the views ; after login comments section shows;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,14 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+        {{-- Bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <livewire:styles />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -32,5 +38,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <livewire:scripts />
     </body>
 </html>
